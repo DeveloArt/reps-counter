c04b8e44015e22d444b128f321606ab1a1aebd56
Nikodem Smolinski <n.smolinski.089@studms.ug.edu.pl>
2026-03-27 19:35:37 +0100

    hide system navigation bar

--- apps/react-native/app/_layout.tsx
+++ apps/react-native/app/_layout.tsx
@@ -2,11 +2,23 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from '../src/hooks/useTheme';
 import '../src/i18n';
-
+import { useEffect } from 'react';
+import { Platform, AppState } from 'react-native';
+import * as NavigationBar from 'expo-navigation-bar';
 export default function RootLayout() {
+  useEffect(() => {
+    if(Platform.OS !== 'android') return;
+    NavigationBar.setVisibilityAsync('hidden');
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if(nextAppState === 'active') {
+        NavigationBar.setVisibilityAsync('hidden');
+      }
+    });
+    return () => subscription.remove();
+  }, []);
   return (
     <ThemeProvider>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" />
         <Stack.Screen
